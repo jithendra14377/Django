2580313d5fad3298517b128889165ee3828863a2
jithendra14377 <trjr0905@gmail.com>
2026-08-13 16:42:21 +0530

    calculatorapp

--- day 3/djangotemplates/djangotemplates/urls.py
+++ day 3/djangotemplates/djangotemplates/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import home,calculator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home,name='home'),
+    path('calculator/',calculator,name='calculator')
 ]
--- day 3/djangotemplates/myapp/views.py
+++ day 3/djangotemplates/myapp/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request,'myapp/home.html')
+
+def calculator(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        a=int(request.POST['a'])
+        b = int(request.POST['b'])
+        op = request.POST['op']
+        c=0
+        if op == 'add':
+            c = a + b
+        elif op == 'sub':
+            c = a- b
+        elif op == 'div':
+            c = a/ b
+        elif op == 'mul':
+            c = a * b
+        context = {'name': name, 'a': a, 'b': b,'c':c,'op':op}
+        return render(request,'myapp/calcresponse.html',context)
+    return render(request,'myapp/calculator.html')
